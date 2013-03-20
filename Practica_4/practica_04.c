5eee8ca72936e67b129983d1ecbe2ed96dd57c82
Mithrandir0x <mithrandir0x@gmail.com>
2013-03-20 15:28:49 +0100

    Added more comments to Practica_04

--- Practica_4/practica_04.c
+++ Practica_4/practica_04.c
@@ -3,32 +3,58 @@
 
 typedef unsigned char byte;
 
+// SMCLK should be used for comm stuff
+// ACLK is recommended to be used for any other things
+
 void initialize_ucs()
 {
+    unsigned short FLLN = 100;
+
 	__bis_SR_register(SCG0); // Toggle the Frequency Locked Loop (Should disable FLL)
 							 // More information at page 30 (Operating Modes)
 
-	UCSCTL0 = 0; // DCO = 0, MOD = 0
-	UCSCTL1 = DCORSEL_7; // DCORSEL = 7 (111)
+	//UCSCTL0 = 0x0000 + ( BIT8 | BIT9 | BITA | BITB | BITC ); // DCO = 0, MOD = 0
+	UCSCTL0 = 0x0000;
+	UCSCTL1 = DCORSEL_4; // DCORSEL = 7 This sets the frequency rang to [8.5, 19.6] MHz (Page 50 MSP430F5438A Datasheet)
 			// This means that the FLL reference is XT2CLK (And this is only valid for F543x(A?) models)
 			// Although it may also mean that FLL reference is ~16MHz (??)
 
-	UCSCTL2 = 487 + FLLD_1; // f_DCOCLK / 1 (Loop Divider) | FLLN = 487
-	UCSCTL3 = 0; // SELREF = 000 (XT1CLK) FLL Reference select | f_FLLREFCLK / 1 FLL Reference divider
+	UCSCTL2 = FLLN + FLLD_0; // f_DCOCLK / 1 (Loop Divider) | FLLN = 487
+	UCSCTL3 = 0x0000; // SELREF = 000 (XT1CLK) FLL Reference select | f_FLLREFCLK / 1 FLL Reference divider
+
+	// Up to here, we have configured the clock to be isuing a signal of ~16Mhz.
+	// (Exactly should be 16,023,552 Hz)
+	// Exactly, this would be:
+	//   1 * ( 100 + 1 ) * ( 32768 / 1 )
+	//   FLLD
+	//         FLLN
+	//                       XT1 DEFAULT Hz VALUE
+	//                               FLLREFDIV
+
+	// The default configuration of the signal is:
+	//   2 * ( 31 + 1 ) * ( 32768 / 1 ) ~= 2MHz
 
 	UCSCTL4 = SELA__XT1CLK     // Alternative Clock source comes from XT1 Oscillator
 			| SELS__DCOCLKDIV  // Subsystem Master Clock source comes from Digital-Controlled Oscillator (Divided)
-			| SELM__DCOCLKDIV; // Master Clock source comes from Digital-Controlled Oscillator (Divided)
+			| SELM__DCOCLK; // Master Clock source comes from Digital-Controlled Oscillator (Divided)
 
-	UCSCTL5 = DIVA_1 // Selects ACLK source divider to 1
-			| DIVS_1 // Selects SMCLK source divider to 1
-			| DIVM_1;// Selects MCLK source divider to 1 (??)
+	// Here we divide the frequency value by a specific value:
+	UCSCTL5 = DIVA_0 // Selects ACLK source divider to 2
+			| DIVS_1 // Selects SMCLK source divider to 2
+			| DIVM_0; // Selects MCLK source divider to 1
 
 	__bis_SR_register(SCG0);  // Toggle the Frequency Locked Loop (Should enable FLL)
 
+	// Notice that P11 only has 3 lines, and its going to output all clock
+	// source frequencies to verify if they're valid
 	P11DIR = 0x07; // P11.0 to P11.2 are selected as OUTPUT
-	P11SEL = 0x07; // ACLK, MCLK and SMCLK
+	P11SEL = 0x07; // P11.0 = ACLK, P11.1 = SMCLK and P11.2 = MCLK
 }
+
+// You can roughly calculate the frequency required to communicate with the AX-12
+// by its bandwidth requirement:
+//
+//       1Mbps ~= 1MHz
 
 void initialize_uart()
 {
@@ -47,6 +73,8 @@ void initialize_uart()
 	UCA0BRW = 1; // Sets baud rate to 1 (??)
 
 	UCA0MCTL = UCOS16; // Enable oversampling mode, but do not select any kind of modulation
+	                   // This allows to transmit with 16MHz, but allows to receive with 1Mhz
+	                   // (because AX-12 works at 1Mhz)
 
 	P3SEL |= 0x30; // Port lines P3.4 and P3.5 selected as default I/O function
 		// P3.4 = UART0TX
@@ -91,4 +119,9 @@ typedef struct {
 
 void main()
 {
+    initialize_ucs();
+
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer (good dog)
+
+    while (1);
 }
